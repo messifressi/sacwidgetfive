d4f338104e4636a974a6dab3e2256026a3e51b51
messifressi <67953592+messifressi@users.noreply.github.com>
2021-03-26 16:35:20 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -321,43 +321,7 @@ d3Script.onload = () =>
 		.attr("height", this._widgetHeight);
 	    	}
 		
-		window._d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
-
-  // When reading the csv, I must format variables:
-  function(d){
-    return { date : window._d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-  },
-
-  // Now I can use this dataset:
-  function(data) {
-
-    // Add X axis --> it is a date format
-    var x = window._d3.scaleTime()
-      .domain(window._d3.extent(data, function(d) { return d.date; }))
-      .range([ 0, 50 ]);
-    this._svgContainer.append("g")
-      .attr("transform", "translate(0,15)")
-      .call(window._d3.axisBottom(x));
-
-    // Add Y axis
-    var y = window._d3.scaleLinear()
-      .domain([0, window._d3.max(data, function(d) { return +d.value; })])
-      .range([ 15, 0 ]);
-    this._svgContainer.append("g")
-      .call(window._d3.axisLeft(y));
-
-    // Add the line
-    this._svgContainer.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", window._d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-
-})
+		
 		
 		this._ksOpenElem.innerHTML = this._ksOpen;
 		this._paxKumValElem.innerHTML = this._paxKumVal;
