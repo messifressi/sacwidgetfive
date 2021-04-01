b563a041d710ae51d65ba668afa63870652c4003
messifressi <67953592+messifressi@users.noreply.github.com>
2021-04-01 15:09:36 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -324,15 +324,15 @@ d3Script.onload = () =>
 		console.log(this._widgetWidth);
 
 
-		var data = [{date: new Date("2021-03-24"), value: parseFloat('65.35')},
-			   {date: new Date("2021-03-25"), value: parseFloat('75.35')},
-			   {date: new Date("2021-03-26"), value: parseFloat('85.35')},
-			   {date: new Date("2021-03-27"), value: parseFloat('95.35')},
-			   {date: new Date("2021-03-28"), value: parseFloat('115.35')}];
+		var data = [{date: "2021-03-24", value: parseFloat('65.35')},
+			   {date: "2021-03-25", value: parseFloat('75.35')},
+			   {date: "2021-03-26", value: parseFloat('85.35')},
+			   {date: "2021-03-27", value: parseFloat('95.35')},
+			   {date: "2021-03-28", value: parseFloat('115.35')}];
 		
 		console.log(data);
 		
-		var parseTime = window._d3.timeParse("%d-%m-%y");
+		var parseTime = window._d3.timeParse("%y-%m-%d");
 		
 		var xScale = window._d3.scaleTime().range([0, this._widgetWidth]);
 		var yScale = window._d3.scaleLinear().range([this._widgetHeight, 0]);
