0f9cd6c1d0a145f11f55176256405af75a835020
messifressi <67953592+messifressi@users.noreply.github.com>
2021-04-01 10:19:10 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -321,22 +321,43 @@ d3Script.onload = () =>
 		.attr("height", this._widgetHeight);
 	    	}
 		
-		//y-axis
-		
-		var y = d3.scaleLinear()
-	      .range([ 15, 0 ]);
-	    var yaxis = this._svgContainer.append("g")
-	      .call(d3.axisLeft(y));
-		
-		//x-axis
-		var x = d3.scaleTime()
-	      .range([ 0, 50 ]);
-	    var yaxis = this._svgContainer.append("g")
-	      .attr("transform", "translate(0,15)")
-	      .call(d3.axisBottom(x));
-		
-		
-		//line
+		 d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+
+		  // When reading the csv, I must format variables:
+		  function(d){
+		    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+		  },
+
+		  // Now I can use this dataset:
+		  function(data) {
+
+		    // Add X axis --> it is a date format
+		    var x = d3.scaleTime()
+		      .domain(d3.extent(data, function(d) { return d.date; }))
+		      .range([ 0, width ]);
+		    svg.append("g")
+		      .attr("transform", "translate(0," + height + ")")
+		      .call(d3.axisBottom(x));
+
+		    // Add Y axis
+		    var y = d3.scaleLinear()
+		      .domain([0, d3.max(data, function(d) { return +d.value; })])
+		      .range([ height, 0 ]);
+		    svg.append("g")
+		      .call(d3.axisLeft(y));
+
+		    // Add the line
+		    svg.append("path")
+		      .datum(data)
+		      .attr("fill", "none")
+		      .attr("stroke", "steelblue")
+		      .attr("stroke-width", 1.5)
+		      .attr("d", d3.line()
+			.x(function(d) { return x(d.date) })
+			.y(function(d) { return y(d.value) })
+			)
+
+		})
 		
 		
 		this._ksOpenElem.innerHTML = this._ksOpen;
